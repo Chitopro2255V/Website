6cb2b11f11be626101755afd00acad8564d1dabc
Your Name <you@example.com>
2025-01-09 19:50:03 -0600

    k

---
+++ OneDrive/Desktop/Programacion proyectos/HTML and CSS projects codecademy/project 8 codecademy/script.js
@@ -0,0 +1,9 @@
+function myFunction() { 
+    var inputValue = document.getElementById("oilup").value;    
+    var nigg = document.getElementById("oil").value; 
+    document.getElementById("answer").innerHTML = inputValue + " " +nigg;}
+
+function nigg() {
+    var image = document.getElementById("davinci");
+    image.src = "images/1989.jpg"
+}
